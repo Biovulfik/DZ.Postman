c290cbb9e6745a9268b4ded287832c25c99d402b
Neto test <2001107@mail.ru>
2024-04-14 00:32:37 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/yqa4htos3xpw46m8/branch/master?svg=true)](https://ci.appveyor.com/project/Biovulfik/dz-api/branch/master)
---
+++ src/test/java/ru/netology/PostmanEchoApiTest.java
@@ -0,0 +1,23 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class PostmanEchoApiTest {
+    @Test
+    void workPost() {
+        String text = "Hi";
+        given()
+                .baseUri("https://postman-echo.com")
+                .body(text) // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", org.hamcrest.Matchers.equalTo("Hallo"))
+        ;
+    }
+}
